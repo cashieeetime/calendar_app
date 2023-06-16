80b41add6a63cefef16ec761733dcca22c5cb5a0
cashieeetime <cashel.creates@gmail.com>
2023-06-16 12:16:23 -0400

    added new component

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <compare-schedule/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CompareSchedule from '@/components/CompareSchedule.vue'
 
 export default {
   name: 'DashboardView',
   components: {
-    HelloWorld
+    CompareSchedule
   }
 }
 </script>
