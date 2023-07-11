2d9d9b3a1046968881e512df690e44c487937a12
cashieeetime <cashel.creates@gmail.com>
2023-07-11 02:55:00 -0400

    added a new view for debugging purposes

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "calendar" */ '../views/CalendarView.vue')
+  },
+  {
+    path: '/testing',
+    name: 'testing',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "testing" */ '../views/TestingView.vue')
   }
 ]
 
