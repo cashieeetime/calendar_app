5ddb18b9b81567d4c48bd7a27d80a3b52a7da129
cashieeetime <cashel.creates@gmail.com>
2023-07-17 02:37:26 -0400

    modified dashboard view

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    
-    <compare-schedule/>
+  <div class="d-flex flex-row justify-space-around">
+    <v-container max-width="500">
+      <to-do-list/>
+    </v-container>
+
+    <v-container>
+      <day-view/>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import CompareSchedule from '@/components/CompareSchedule.vue'
+// import CompareSchedule from '@/components/CompareSchedule.vue'
+import ToDoList from '@/components/ToDoList.vue'
+import DayView from '@/components/DayView.vue'
 
 export default {
   name: 'DashboardView',
   components: {
-    CompareSchedule
+    // CompareSchedule,
+    ToDoList,
+    DayView
   }
 }
 </script>
