41c3bddd8d920d861642f90b8ae2270b98bded33
cashieeetime <cashel.creates@gmail.com>
2023-07-17 03:38:40 -0400

    messed around in testing view

--- src/views/TestingView.vue
+++ src/views/TestingView.vue
@@ -1,31 +1,46 @@
 <template>
-    <div class="d-flex flex-column mb-6">
-        <v-container>
-            <event-card/>
-        </v-container>
+    <div>
+        <div class="d-flex flex-column justify-center">
+            <v-container>
+                <v-card>
+                    <full-calendar/>
+                </v-card>
+            </v-container>
+        </div>
 
-        <v-container>
-            <drag-drop-calendar/>
-        </v-container>
-        
-        <v-container>
-            <nav-bar/>
-        </v-container>
+        <!--
+        <div class="d-flex flex-column mb-6">
+            <v-container>
+                <event-card/>
+            </v-container>
+            
+            <v-container>
+                <drag-drop-calendar/>
+            </v-container>
+            
+            <v-container>
+                <nav-bar/>
+            </v-container>
+        </div>
+        -->
     </div>
 </template>
   
 <script>
 // @ is an alias to /src
-import EventCard from '@/components/EventCard.vue'
-import DragDropCalendar from '@/components/DragDropCalendar.vue'
-import NavBar from '@/components/NavBar.vue'
+
+// import EventCard from '@/components/EventCard.vue'
+// import DragDropCalendar from '@/components/DragDropCalendar.vue'
+// import NavBar from '@/components/NavBar.vue'
+import FullCalendar from '@/components/FullCalendar.vue'
 
 export default {
 name: 'DashboardView',
 components: {
-    EventCard,
-    DragDropCalendar,
-    NavBar
+    // EventCard,
+    // DragDropCalendar,
+    // NavBar,
+    FullCalendar
     }}
 </script>
   
