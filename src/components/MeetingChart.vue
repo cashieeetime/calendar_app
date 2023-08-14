1860665553bb07c17846a88422498e919c9306e8
cashieeetime <cashel.creates@gmail.com>
2023-08-14 01:13:47 -0400

    commented out & condensed code

--- src/components/MeetingChart.vue
+++ src/components/MeetingChart.vue
@@ -9,7 +9,7 @@
 <script>
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-import {eventData} from '@/eventdata.js'
+// import {eventData} from '@/eventdata.js'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
@@ -18,9 +18,9 @@ export default {
   components: { 
     Bar, 
   },
-  mixins: [eventData],
-  methods: { /*
-    prepRaw (allEvents) {
+  /* mixins: [eventData],
+  methods: {
+    prepData (allEvents) {
       for (let key = 0; key < 1; p++); {
         for (let value = 0; value < allEvents[key], p++;) {
           if (value in chartData) {
@@ -40,19 +40,19 @@ export default {
       }
       return peopleList
     },
-    prepData (allEvents) {
+    prepValues (allEvents) {
       events = prepData (allEvents)
       dataList = []
       for (i = 0; i < allEvents; i++) {
         dataList.push(allEvents[i])
       }
       return dataList
-    } */
-  },
+    } 
+  },  
   created() {
     console.log(this.getEvents())
-    // this.prepRData
-  },
+    this.prepRData
+  }, */
   data() {
     return {
       chartData: {
@@ -64,13 +64,8 @@ export default {
             data: [1, 3, 2, 1, 2],
           }
         ],
-        options: {
-          indexAxis: 'y',
-        }
-
-
-        
       },
+      
     }
   }
 }
