442011242eba19e92fa1de188ba7e9a72d7f0400
cashieeetime <cashel.creates@gmail.com>
2023-08-14 23:15:55 -0400

    edits for presenting

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <v-main>
         <nav>
           <router-link to="/">Dashboard</router-link> | 
-          <router-link to="/calendar">Calendar</router-link> | 
-          <!-- <router-link to="/testing">Testing</router-link> -->
+          <router-link to="/calendar">Calendar</router-link> <!-- | 
+          <router-link to="/testing">Testing</router-link> -->
         </nav>
         <router-view/>
       </v-main>
